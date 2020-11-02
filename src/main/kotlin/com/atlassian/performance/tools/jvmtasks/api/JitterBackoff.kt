6d9017d241f81a809fba11f50fa3635a9ac81a85
Maciej Kwidzinski <mkwidzinski@atlassian.com>
2020-11-02 16:16:12 +0100

    JPERF-683: Fix jitter back-offs returning negative time

--- src/main/kotlin/com/atlassian/performance/tools/jvmtasks/api/JitterBackoff.kt
+++ src/main/kotlin/com/atlassian/performance/tools/jvmtasks/api/JitterBackoff.kt
@@ -2,6 +2,7 @@ package com.atlassian.performance.tools.jvmtasks.api
 
 import java.time.Duration
 import java.util.Random
+import kotlin.math.absoluteValue
 
 /**
  * Backs off for a random duration, at most [max].
@@ -15,7 +16,10 @@ class JitterBackoff(
     private val random = Random()
 
     override fun backOff(attempt: Int): Duration {
-        val randomMillis = random.nextLong().rem(max.toMillis())
+        val randomMillis = random
+            .nextLong()
+            .absoluteValue
+            .rem(max.toMillis())
         return Duration.ofMillis(randomMillis)
     }
 }
