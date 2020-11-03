7bf5d71de671165ce1100d965578517b339cb132
Maciej Kwidzinski <mkwidzinski@atlassian.com>
2020-11-03 09:59:27 +0100

    JPERF-683: Parametrize `JitterBackoff` with a `Random`

--- src/test/kotlin/com/atlassian/performance/tools/jvmtasks/api/JitterBackoffTest.kt
+++ src/test/kotlin/com/atlassian/performance/tools/jvmtasks/api/JitterBackoffTest.kt
@@ -2,13 +2,15 @@ package com.atlassian.performance.tools.jvmtasks.api
 
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.Test
+import java.time.Duration.ofMillis
 import java.time.Duration.ofSeconds
+import java.util.Random
 
 class JitterBackoffTest {
 
     @Test
     fun shouldYieldPositiveTime() {
-        val backoff = JitterBackoff(ofSeconds(10))
+        val backoff = JitterBackoff(ofSeconds(10), Random(888))
 
         val durations = (1..1000).map { backoff.backOff(1) }
 
@@ -17,11 +19,11 @@ class JitterBackoffTest {
 
     @Test
     fun shouldSpread() {
-        val backoff = JitterBackoff(ofSeconds(10))
+        val backoff = JitterBackoff(ofSeconds(10), Random(555))
 
         val durations = (1..1000).map { backoff.backOff(1) }
 
         val spread = durations.max()!! - durations.min()!!
-        assertThat(spread).isGreaterThan(ofSeconds(7)) // yeah there's a chance it flakes, but it's 1 in gazillion
+        assertThat(spread).isGreaterThan(ofSeconds(9) + ofMillis(500))
     }
 }
